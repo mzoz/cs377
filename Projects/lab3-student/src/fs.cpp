66d2477447b3f44f05fe133d779135377062bbd1
Tony Gao <zijiangao@umass.edu>
2017-05-13 11:53:21 -0700

    adding finished lab 3, end of semester wrap up

--- Projects/lab3-student/src/fs.cpp
+++ Projects/lab3-student/src/fs.cpp
@@ -1,16 +1,40 @@
 #include "fs.h"
+#include <math.h> 
+#include <vector>
+#include <iostream>
+#include <signal.h>
+
+#define DEBUG 1
 
 using namespace std;
+
+typedef struct idxNode idxNode;
 
 myFileSystem::myFileSystem(char diskName[16]) {
   // open the file with the above name
   // this file will act as the "disk" for your file system
+  this->disk.open(diskName, ios::out | ios::in);
 }
 
 int myFileSystem::create_file(char name[8], int size) {
+  if (!this->disk.is_open())
+    return -1; //error disk not open
+
+  if (size > 8 || size < 1)
+    return -1; //size too large
+
+  if (strlen(name) > 8 || strlen(name) == 0)
+    return -1; // invalid name
+
   //create a file with this name and this size
 
   // high level pseudo code for creating a new file
+
+  #if DEBUG
+
+  cout << "Attempting to create file: " << name << endl;
+
+  #endif
 
   // Step 1: Check to see if we have sufficient free space on disk by
   //   reading in the free block list. To do this:
@@ -18,6 +42,31 @@ int myFileSystem::create_file(char name[8], int size) {
   // Read the first 128 bytes (the free/in-use block information)
   // Scan the list to make sure you have sufficient free blocks to
   //   allocate a new file of this size
+
+  this->disk.sync();
+
+  this->disk.seekg(0, disk.beg); //move file ptr
+  char buf_free[128];
+
+  this->disk.read(buf_free, 128); //read first 128 bytes, free block list
+
+  vector<int> blocks_free; // to be filled with free block numbers
+
+  for (int i = 1; i < 128; i ++){
+    // scan list
+    if (buf_free[i] == 0)
+      blocks_free.push_back(i);
+    if (blocks_free.size() >= (size_t)size)
+      break;
+  }
+
+  if (blocks_free.size() == 0 || blocks_free.size() < (size_t)size) {
+    #if DEBUG
+    cout << "Error insufficient free blocks available for creation\n";
+    #endif
+    
+    return -1; //error, scanned entire free list and insufficient blocks
+  }
 
   // Step 2: we look for a free inode on disk
   // Read in an inode
@@ -27,6 +76,45 @@ int myFileSystem::create_file(char name[8], int size) {
   // Copy the filename to the "name" field
   // Copy the file size (in units of blocks) to the "size" field
 
+  idxNode found_inode;
+  int inode_pos = -1;
+  // i'th inode is located at 128 + 48 * i
+  for(int i = 0; i < 16; i ++){ //look for free inode, there are 16
+
+    // begin read in inode
+    char tmp_buf[48];
+    this->disk.read(tmp_buf, 48);
+    //deserialize struct
+    memcpy(&found_inode, tmp_buf, sizeof(found_inode));
+
+    // end read in inode
+
+    if (found_inode.used == 0){ // check "used" field
+      inode_pos = i;
+      break;
+    } /*else if(strncmp(name, found_inode.name, 8) == 0){
+      #if DEBUG
+      cout << "Error in create, file with the same name exists\n";
+      #endif
+
+      return -1;
+    }  */
+    else if (i == 15){
+      #if DEBUG
+      cout << "Error in create, no free inodes\n";
+      #endif
+
+      return -1; //error, no free inodes
+    }
+  }
+
+  if (inode_pos == -1)
+    return -1;
+
+  found_inode.used = 1; //set the used field to 1
+  strncpy(found_inode.name, name, 8); //copy the filename to the name field
+  found_inode.size = size; //copy the file size
+
   // Step 3: Allocate data blocks to the file
   // for(i=0;i<size;i++)
   // Scan the block list that you read in Step 1 for a free block
@@ -34,15 +122,33 @@ int myFileSystem::create_file(char name[8], int size) {
   // Set the blockPointer[i] field in the inode to this block number.
   // end for
 
+  for (int i = 0; i < size; i ++){
+    found_inode.blockPointers[i] = blocks_free[i];
+    buf_free[blocks_free[i]] = 1;
+  }
+
   // Step 4: Write out the inode and free block list to disk
   // Move the file pointer to the start of the file
   // Write out the 128 byte free block list
   // Move the file pointer to the position on disk where this inode was stored
   // Write out the inode
-  return 0;
+
+  this->disk.seekp(0, disk.beg); //move file ptr to start
+  this->disk.write(buf_free, 128); // write out free block list
+  this->disk.seekp(48 * inode_pos, disk.cur); // seek to inode
+
+  char tmp_buf[48];
+  memcpy(tmp_buf, &found_inode, sizeof(found_inode)); //serialize inode
+
+  this->disk.write(tmp_buf, 48); // write out inode
+  this->disk.flush();
+
+  return 1;
 } // End Create
 
 int myFileSystem::delete_file(char name[8]){
+  if (!this->disk.is_open())
+    return -1; //error disk not open
   // Delete the file with this name
 
   // Step 1: Locate the inode for this file
@@ -53,6 +159,47 @@ int myFileSystem::delete_file(char name[8]){
   //   inode matches the file we want to delete. If not, read the next
   //   inode and repeat
 
+  #if DEBUG
+
+  cout << "Deleting file : " << name << endl;
+
+  #endif
+
+  this->disk.sync();
+
+  this->disk.seekg(128, disk.beg);
+
+  idxNode found_inode;
+  int inode_pos = -1;
+
+  for(int i = 0; i < 16; i ++){
+
+    // begin read in inode
+    
+    char tmp_buf[48];
+    this->disk.read(tmp_buf, 48);
+
+    //deserialize struct, copy buf to struct
+    memcpy(&found_inode, tmp_buf, sizeof(found_inode));
+
+    // end read in inode
+
+    if (found_inode.used == 1 && strncmp(found_inode.name, name, 8) == 0 && i != 15){ // check "used" field
+      inode_pos = i; //i < 15, found matching inode
+      break;
+    } else if (i == 15){
+      if (found_inode.used && strncmp(found_inode.name, name, 8) == 0){
+        inode_pos = i; //reached end, found matching inode
+        break;
+      } else {
+        #if DEBUG
+        cout << "Error in delete_file, no matching inode, all nodes scanned\n";
+        #endif
+        return -1;
+      } // error, searched all files, non existant
+    }
+  }
+
   // Step 2: free blocks of the file being deleted
   // Read in the 128 byte free block list (move file pointer to start
   //   of the disk and read in 128 bytes)
@@ -60,19 +207,42 @@ int myFileSystem::delete_file(char name[8]){
   // for(i=0;i< inode.size; i++)
   // freeblockList[ inode.blockPointer[i] ] = 0;
 
+  this->disk.seekg(0, disk.beg); //move file ptr
+  char buf_free[128];
+
+  this->disk.read(buf_free, 128); //read first 128 bytes, free block list
+
+  for (int i = 0; i < found_inode.size; i ++){
+    buf_free[found_inode.blockPointers[i]] = 0; //free blocks of found file
+  }
+
   // Step 3: mark inode as free
   // Set the "used" field to 0.
+
+  found_inode.used = 0;
 
   // Step 4: Write out the inode and free block list to disk
   // Move the file pointer to the start of the file
   // Write out the 128 byte free block list
   // Move the file pointer to the position on disk where this inode was stored
   // Write out the inode
-  return 0;
+
+  this->disk.seekp(0, disk.beg); //move file ptr to start
+  this->disk.write(buf_free, 128); // write out free block list
+  this->disk.seekp(48 * inode_pos, disk.cur); // seek to inode
+
+  char tmp_buf[48];
+  memcpy(tmp_buf, &found_inode, sizeof(found_inode)); //serialize inode
+
+  this->disk.write(tmp_buf, 48); // write out inode
+  this->disk.flush();
+  return 1;
 } // End Delete
 
 
 int myFileSystem::ls(){
+  if (!this->disk.is_open())
+    return -1; //error disk not open
   // List names of all files on disk
 
   // Step 1: read in each inode and print
@@ -82,10 +252,35 @@ int myFileSystem::ls(){
   // If the inode is in-use
   // print the "name" and "size" fields from the inode
   // end for
-    return 0;
+  this->disk.sync();
+
+  this->disk.seekg(128, disk.beg);
+
+  cout << endl << endl << "Listing INodes..." << endl;
+
+  for(int i = 0; i < 16; i ++){
+
+    // begin read in inode
+    idxNode found_inode;
+    char tmp_buf[48];
+    this->disk.read(tmp_buf, 48);
+
+    //deserialize struct, copy buf to struct
+    memcpy(&found_inode, tmp_buf, sizeof(found_inode));
+
+    // end read in inode
+
+    if (found_inode.used == 1){ // check "used" field
+      cout << "INode: " << found_inode.name << " size: " << found_inode.size << endl;
+    }     
+  }
+
+  return 1;
 }; // End ls
 
 int myFileSystem::read(char name[8], int blockNum, char buf[1024]){
+  if (!this->disk.is_open())
+    return -1; //error disk not open
   // read this block from this file
 
   // Step 1: locate the inode for this file
@@ -94,6 +289,47 @@ int myFileSystem::read(char name[8], int blockNum, char buf[1024]){
   // If the inode is in use, compare the "name" field with the above file
   // If the file names don't match, repeat
 
+  #if DEBUG
+
+  cout << "Reading file : " << name << " blockNum: " << blockNum << endl;
+
+  #endif
+
+  this->disk.sync();
+
+  this->disk.seekg(128, disk.beg);
+
+  idxNode found_inode;
+  int inode_pos = -1;
+
+  for(int i = 0; i < 16; i ++){
+
+    // begin read in inode
+    
+    char tmp_buf[48];
+    this->disk.read(tmp_buf, 48);
+
+    //deserialize struct, copy buf to struct
+    memcpy(&found_inode, tmp_buf, sizeof(found_inode));
+
+    // end read in inode
+
+    if (found_inode.used == 1 && strncmp(found_inode.name, name, 8) == 0 && i != 15){ // check "used" field
+      inode_pos = i; //i < 15, found matching inode
+      break;
+    } else if (i == 15){
+      if (found_inode.used && strncmp(found_inode.name, name, 8) == 0){
+        inode_pos = i; //reached end, found matching inode
+        break;
+      } else {
+        #if DEBUG
+        cout << "Error in read_file, no matching inode, all nodes scanned\n";
+        #endif
+        return -1;
+      } // error, searched all files, non existant
+    }
+  }
+
   // Step 2: Read in the specified block
   // Check that blockNum < inode.size, else flag an error
   // Get the disk address of the specified block
@@ -101,13 +337,32 @@ int myFileSystem::read(char name[8], int blockNum, char buf[1024]){
   // Move the file pointer to the block location (i.e., to byte #
   //   addr*1024 in the file)
 
+  if (blockNum >= found_inode.size){
+    #if DEBUG
+    cout << "Error requested blocknum greater than size of inode.\n";
+    #endif
+    return -1;
+  }
+
+  int offset = found_inode.blockPointers[blockNum] * block_size;
+
+  #if DEBUG
+  cout << "Read pointer: " << found_inode.blockPointers[blockNum] << " offset: " 
+        << offset << endl;
+  #endif
   // Read in the block => Read in 1024 bytes from this location
   //   into the buffer "buf"
-  return 0;
+
+  this->disk.seekg(offset, disk.beg);
+  this->disk.read(buf, block_size);
+
+  return 1;
 } // End read
 
 
 int myFileSystem::write(char name[8], int blockNum, char buf[1024]){
+  if (!this->disk.is_open())
+    return -1; //error disk not open
   // write this block to this file
 
   // Step 1: locate the inode for this file
@@ -116,17 +371,79 @@ int myFileSystem::write(char name[8], int blockNum, char buf[1024]){
   // If the inode is in use, compare the "name" field with the above file
   // If the file names don't match, repeat
 
+  #if DEBUG
+
+  cout << "Reading file : " << name << " blockNum: " << blockNum << endl;
+
+  #endif
+
+  this->disk.sync();
+
+  this->disk.seekg(128, disk.beg);
+
+  idxNode found_inode;
+  int inode_pos = -1;
+
+  for(int i = 0; i < 16; i ++){
+
+    // begin read in inode
+    
+    char tmp_buf[48];
+    this->disk.read(tmp_buf, 48);
+
+    //deserialize struct, copy buf to struct
+    memcpy(&found_inode, tmp_buf, sizeof(found_inode));
+
+    // end read in inode
+
+    if (found_inode.used == 1 && strncmp(found_inode.name, name, 8) == 0 && i != 15){ // check "used" field
+      inode_pos = i; //i < 15, found matching inode
+      break;
+    } else if (i == 15){
+      if (found_inode.used && strncmp(found_inode.name, name, 8) == 0){
+        inode_pos = i; //reached end, found matching inode
+        break;
+      } else {
+        #if DEBUG
+        cout << "Error in read_file, no matching inode, all nodes scanned\n";
+        #endif
+        return -1;
+      } // error, searched all files, non existant
+    }
+  }
+
   // Step 2: Write to the specified block
   // Check that blockNum < inode.size, else flag an error
   // Get the disk address of the specified block
   // That is, addr = inode.blockPointer[blockNum]
   // Move the file pointer to the block location (i.e., byte # addr*1024)
 
+  if (blockNum >= found_inode.size){
+    #if DEBUG
+    cout << "Error requested blocknum greater than size of inode.\n";
+    #endif
+    return -1;
+  }
+
+  int offset = found_inode.blockPointers[blockNum] * block_size;
+
+  #if DEBUG
+  cout << "Write pointer: " << found_inode.blockPointers[blockNum] << " offset: " 
+        << offset << endl;
+  #endif
+
   // Write the block! => Write 1024 bytes from the buffer "buff" to
   //   this location
-  return 0;
+
+  this->disk.seekp(offset, disk.beg);
+  this->disk.write(buf, block_size);
+
+  this->disk.flush();
+
+  return 1;
 } // end write
 
 int myFileSystem::close_disk(){
-    return 0;
+    disk.close();
+    return 1;
 }
--- Projects/lab3-student/test/fs_tests.cpp
+++ Projects/lab3-student/test/fs_tests.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <fstream>
+#include <sstream>
 
 #include "../src/fs.h"
 
@@ -16,6 +17,10 @@ protected:
     system("./create_fs disk0");
     system("./create_fs disk1");
   }
+
+  ~FSTest() {
+    cout << "_________________________________" << endl;
+  }
 };
 
 //test create_file return code 1 for success
@@ -23,15 +28,98 @@ TEST_F(FSTest, create_file_test) {
 	myFileSystem f ((char*) "disk0");
 	int code = f.create_file((char*)"test.c",1);
 	ASSERT_EQ(1,code);
+
+  f.ls();
 }
 
 //test delete_file return code -1 for failure
-TEST_F(FSTest, delete_file_test) {
+TEST_F(FSTest, delete_file_test_negative) {
 	myFileSystem f ((char*) "disk0");
 	int code = f.delete_file((char*)"test.c");
 	ASSERT_EQ(-1,code);
+
+  f.ls();
 }
 
+TEST_F(FSTest, delete_file_test_positive) {
+  myFileSystem f ((char*) "disk0");
+  int code = f.create_file((char*)"test.c",1);
+  ASSERT_EQ(1,code);
+  code = f.delete_file((char*)"test.c");
+  ASSERT_EQ(1,code);
+
+  f.ls();
+}
+
+TEST_F(FSTest, read_file_test_positive) {
+  myFileSystem f ((char*) "disk0");
+  int code = f.create_file((char*)"test.c",1);
+  ASSERT_EQ(1,code);
+  char buf[1024];
+  code = f.read("test.c", 0, buf);
+
+  char buf_expected[1024] = {0};
+
+  for (int i = 0; i < 1024; i ++)
+    ASSERT_EQ(buf[i], buf_expected[i]);
+
+  ASSERT_EQ(1,code);
+
+  buf[0] = 1;
+  code = f.write("test.c", 0, buf);
+
+  char new_buf[1024];
+  code = f.read("test.c", 0, new_buf);
+
+  ASSERT_EQ(new_buf[0], 1);
+
+  for (int i = 1; i < 1024; i ++)
+    ASSERT_EQ(new_buf[i], 0);
+
+  f.ls();
+}
+
+TEST_F(FSTest, create_file_no_freeblocks_test){
+  myFileSystem f ((char*) "disk0");
+  int code = f.create_file((char*)"t0",8);
+  ASSERT_EQ(1,code);
+  string filename = "t";
+
+  for(int i = 1; i < 15; i ++){
+    std::ostringstream oss;
+    oss << filename << i;
+    code = f.create_file(strdup(oss.str().c_str()),8);
+    ASSERT_EQ(1,code);
+  }
+
+  code = f.create_file("lastone",8);
+
+  f.ls();
+  
+  ASSERT_EQ(-1, code);
+
+  
+  
+}
+
+TEST_F(FSTest, create_file_max_num_files_test){
+  myFileSystem f ((char*) "disk0");
+  int code = f.create_file((char*)"t0",8);
+  ASSERT_EQ(1,code);
+  string filename = "t";
+
+  for(int i = 1; i < 16; i ++){
+    std::ostringstream oss;
+    oss << filename << i;
+    code = f.create_file(strdup(oss.str().c_str()),1);
+    ASSERT_EQ(1,code);
+  }
+
+  f.ls();
+
+  code = f.create_file("nope", 2);
+  ASSERT_EQ(-1, code);
+}
 
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
